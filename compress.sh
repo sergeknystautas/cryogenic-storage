487292236df0bd31cc289598addb1687ce2a27b6
Serge Knystautas <sergek@lokitech.com>
2020-11-29 23:15:55 -0800

    Prep 0.1.1 for support for Factorio 1.1

--- compress.sh
+++ compress.sh
@@ -1,3 +1,3 @@
-rm ~/Library/Application\ Support/factorio/mods/cryogenic-storage_0.1.0.zip
-cd ..; zip -r -X ~/Library/Application\ Support/factorio/mods/cryogenic-storage_0.1.0.zip cryogenic-storage -x compress.sh \*.DS_Store
+rm ~/Library/Application\ Support/factorio/mods/cryogenic-storage_0.1.1.zip
+cd ..; zip -r -X ~/Library/Application\ Support/factorio/mods/cryogenic-storage_0.1.1.zip cryogenic-storage -x compress.sh \*.DS_Store
 cd cryogenic-storage
